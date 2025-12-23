051a66f931658609427dcc5e77a05cb28ccf95a3
muu-ima <enyukari.k.imamura@gmail.com>
2025-12-23 13:49:49 +0900

    Revert "feat: スナップショット共有URL (/s/[token]) を実装"
    This reverts commit ad236f65aaf3e9f75e6ad87edef38b125a406f9e.

--- lib/snapshot.types.ts
+++
@@ -1,9 +0,0 @@
-// lib/snapshot.types.ts
-import type { DesignKey } from "@/shared/design";
-import type { Block } from "@/shared/blocks";
-
-export type SnapshotPayload = {
-  version: 1;
-  design: DesignKey;
-  blocks: Block[];
-};
